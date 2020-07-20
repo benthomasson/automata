c6f56e7d4017961e196f001f74455192adecfabc
Ben Thomasson <bthomass@redhat.com>
2020-07-20 10:38:42 -0400

    Improving tests

--- fsm/fsm_test.go
+++ fsm/fsm_test.go
@@ -24,6 +24,16 @@ func noop1(*fsm.State, *fsm.Controller) {
 func noop2(*fsm.State, *fsm.Controller, fsm.Message) {
 }
 
+var value int = 1;
+
+func setValue1(*fsm.State, *fsm.Controller) {
+    value = 4
+}
+
+func setValue2(*fsm.State, *fsm.Controller, fsm.Message) {
+    value = 5
+}
+
 func Test1(t *testing.T) {
     ch := make(chan fsm.Message, 2)
     ch <- fsm.SimpleMessage{MessageTypeName: "foo1"}
@@ -50,37 +60,81 @@ func Test2(t *testing.T) {
 
 func TestState(t *testing.T) {
 
-    s := &fsm.State{Enter: noop1, Exit: noop1}
+    value = 1
+
+    s := &fsm.State{Enter: noop1, Exit: setValue1}
     c := &fsm.Controller{State: s}
     c.ChangeState(s)
-    c.ChangeState(s)
-    describe(s)
-    describe(c)
+    if c.State != s {
+        t.Errorf("Wrong state")
+    }
+    if value != 4 {
+        t.Errorf("Exit handler not run")
+    }
 }
 
 func TestMessage(t *testing.T) {
 
-    s := &fsm.State{Enter: noop1, Exit: noop1}
+    value = 1
+    handlers := map[string]func(*fsm.State, *fsm.Controller, fsm.Message){
+        "SimpleMessage": setValue2,
+    }
+
+    s := &fsm.State{Enter: setValue1, Exit: noop1, Handlers: handlers}
     c := &fsm.Controller{State: s}
     c.ChangeState(s)
-    c.HandleMessage(fsm.SimpleMessage{})
-    describe(s)
-    describe(c)
-}
+    if c.State != s {
+        t.Errorf("Wrong state")
+    }
 
+    if value != 4 {
+        t.Errorf("Enter handler not run")
+    }
+
+    c.HandleMessage(fsm.SimpleMessage{MessageTypeName: "SimpleMessage"})
+
+    if value != 5 {
+        t.Errorf("Message handler not run")
+    }
+}
 
 
 func TestReceiveMessages(t *testing.T) {
 
-    s := &fsm.State{Enter: noop1, Exit: noop1}
+    value = 1
+    handlers := map[string]func(*fsm.State, *fsm.Controller, fsm.Message){
+        "SimpleMessage": setValue2,
+    }
+
+    s := &fsm.State{Enter: setValue1, Exit: noop1, Handlers: handlers}
     c := &fsm.Controller{State: s}
     c.ChangeState(s)
+    if c.State != s {
+        t.Errorf("Wrong state")
+    }
+    if value != 4 {
+        t.Errorf("Enter handler not run")
+    }
     ch := make(chan fsm.Message, 1)
     go c.ReceiveMessages(ch)
-    ch <- fsm.SimpleMessage{}
-    describe(s)
-    describe(c)
+    ch <- fsm.SimpleMessage{MessageTypeName: "SimpleMessage"}
     time.Sleep(1 * time.Millisecond)
+    if value != 5 {
+        t.Errorf("Message handler not run")
+    }
+}
+
+func TestValue(t *testing.T) {
+
+    value = 1 
+
+    if value != 1 {
+        t.Errorf("wrong value")
+    }
+    value = 2
+    if value != 2 {
+        t.Errorf("wrong value")
+    }
 }
 
 
